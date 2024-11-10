399cf88d45157970510795fbb1d4cea43d564af1
Enggar <enggarprathitaj@gmail.com>
2024-11-10 16:22:30 +0700

    init

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -2,16 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 
 import { Button, Card, CardBody, Container, FormGroup, Label, Input } from 'reactstrap'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  
+
   return (
     <>
-      <Container className="mt-5 w-50">
-        <Card>
+      <Container className="d-flex justify-content-center align-items-center mt-5" style={{ height: '100vh' }}>
+        <Card className='w-50'>
           <CardBody>
             <h4>Contact Us</h4>
             <FormGroup>
@@ -33,6 +36,9 @@ function App() {
               <Label for="message">Message</Label>
               <Input type="textarea" name="message" />
             </FormGroup>
+
+
+            <Button color="primary" block>Submit</Button>
 
 
           </CardBody>
